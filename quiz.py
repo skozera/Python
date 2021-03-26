e4c3ddafa8d73ee48e6503df6cedc6d2ef015812
Sylweste Kozera <sylwester.kozera@gmail.com>
2021-03-26 22:10:36 +0100

    Removed sigup & signin defs

--- quiz.py
+++ quiz.py
@@ -5,51 +5,12 @@ Created on Wed Mar 24 18:31:00 2021
 
 import random
 import time
-import pathlib
-import hashlib
-import operator
 
 level = 1
 score = 1 # inittial points
 rounds = 3 # how many rounds do you want to play
 limit = 30 # max result of addition or subtraction
 prize = 10 # points for every corect oraz uncorrect (with minus) answear
-
-def sign_up():
-    username = input("Enter a username: ")
-    while True:
-        password = input("Enter a password (6 character long): ")
-        if len(password) < 6:
-            password = input("Enter a password (6 character long): ")
-        else:
-            break
-    hashed_password = hashlib.sha1(password.encode("utf-8")).hexdigest()
-    # print(hashed_password)
-    with open("credentials.txt", mode="w") as writable_password:
-        writable_password.write(username+"\n")
-        writable_password.write(hashed_password+"\n")
-
-    log_in()
-
-def log_in():
-    print("Please, enter your usarname and password to sign in")
-    username = input("Username: ")+"\n"
-
-    while True:
-        password = input("Password (6 character long): ")
-        if len(password) < 6:
-            password = input("Password (6 character long): ")
-        else:
-            break
-
-    with open(".credentials.txt", mode="r") as credentials:
-        user_data = credentials.readlines()
-        for i in user_data:
-            # print(i)
-            if i == username and hashlib.sha1(password.encode("utf-8")).hexdigest()+"\n" == user_data[user_data.index(i)+1]:
-                print("You have succesfully signed in")
-                start_game()
-                break
 
 def start_game(limit):
     start = "ll"
@@ -115,6 +76,5 @@ def level_one(limit):
 
 # if __name__ = "__main__":
 #     main()
-# sign_up()
-# log_in()
+
 start_game(limit)
